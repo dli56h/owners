f12c5d457f7280622100704824e1dac8a0ad647b
dilip <draibahing@gmail.con>
2025-08-14 16:37:44 +0545

    Add multi function in cpp

--- mul.cpp
+++ mul.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+double multi(double a, double b) {
+    return a * b;
+}
+
 int main() {
-    cout << "Hello world";
+    cout << "Hello world\n\n";
+    cout << "multiplication = " << multi(23, 1);
+
     return 0;
 }
